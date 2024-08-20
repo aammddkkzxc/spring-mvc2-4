2752ad6b763aa86a65217340cbff22f630991fe2
aammddkkzxc <ideal0427@naver.com>
2024-08-21 05:40:10 +0900

    feat: Validator 분리2 - @InitBinder => 해당 컨트롤러에만 영향을 준다. 글로벌 설정은 별도로 해야한다 - @Validated 는 검증기를 실행하라는 애노테이션 (스프링 전용 검증 애노테이션,  @Valid 는 자바 표준 검증 애노테이션)

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -11,6 +11,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -26,6 +28,12 @@ public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
     private final ItemValidator itemValidator;
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        log.info("init binder {}", dataBinder);
+        dataBinder.setValidator(itemValidator);
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -197,16 +205,32 @@ public class ValidationItemControllerV2 {
 //        return "redirect:/validation/v2/items/{itemId}";
 //    }
 
-    @PostMapping("/add")
-    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult,
-                            RedirectAttributes redirectAttributes) {
+//    @PostMapping("/add")
+//    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult,
+//                            RedirectAttributes redirectAttributes) {
+//
+//        itemValidator.validate(item, bindingResult);
+//
+//        if (bindingResult.hasErrors()) {
+//            log.info("errors={}", bindingResult);
+//            return "validation/v2/addForm";
+//        }
+//        //성공 로직
+//        Item savedItem = itemRepository.save(item);
+//        redirectAttributes.addAttribute("itemId", savedItem.getId());
+//        redirectAttributes.addAttribute("status", true);
+//        return "redirect:/validation/v2/items/{itemId}";
+//    }
 
-        itemValidator.validate(item, bindingResult);
+    @PostMapping("/add")
+    public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes) {
 
         if (bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
             return "validation/v2/addForm";
         }
+
         //성공 로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
